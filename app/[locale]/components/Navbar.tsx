80ba05c6efcc55288e9af51fdac7d6f8d374090c
asadmasoodchaudhry99 <asadmchaudhry0@gmail.com>
2025-02-26 02:44:30 +0100

    FluentUI use, Added useLocale, remove debug output

--- app/[locale]/components/Navbar.tsx
+++ app/[locale]/components/Navbar.tsx
@@ -1,33 +1,26 @@
 import React, { useState, useEffect } from "react";
 import {Button} from "@fluentui/react-button";
+import {Select} from "@fluentui/react-select";
+import {useId} from "@fluentui/react-utilities";
 import {useDarkModeContext} from "./Setup";
 import {
     DarkThemeFilled, HomeRegular,
     SettingsRegular,
 } from "@fluentui/react-icons";
-import {useRouter, usePathname} from "next/navigation";
-import { useTranslations } from "next-intl";
+import { usePathname} from "next/navigation";
+import { useLocale, useTranslations } from "next-intl";
 
-const Navbar = ({ locale }: { locale: string }) => {
-    const [currentLocale, setCurrentLocale] = useState(locale);
+const Navbar = () => {
+    const locale = useLocale(); 
     const {darkMode, setDarkMode} = useDarkModeContext();
+    const selectId = useId();
     const t = useTranslations("Navbar");
-    const router = useRouter();
     const pathname = usePathname()
     const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const newLocale = e.target.value;
-        setCurrentLocale(newLocale);
         const path = pathname.split("/").slice(2).join("/"); 
         window.location.href = (`/${newLocale}/${path}`); 
     };
-    useEffect(() => {
-        // Keep the locale in sync with the path
-        const currentLocale = pathname.split("/")[1];
-        console.log(currentLocale)
-        if (currentLocale !== locale) {
-            setCurrentLocale(currentLocale);
-        }
-      }, [pathname]);
     
     return <div className="relative bg-blue-950 h-12">
         <div className="h-full flex items-center px-3 justify-between">
@@ -55,15 +48,14 @@ const Navbar = ({ locale }: { locale: string }) => {
                         onClick={() => setDarkMode(!darkMode)}></Button>
             </div>
             <div>
-            <select
-        value={currentLocale}
+            <Select id={selectId} 
+        value={locale}
         onChange={handleLanguageChange}
-        className={"text-white rounded-md px-4 py-2 bg-transparent hover:outline-none focus:outline-none"}
-    
+        className={"text-white rounded-md py-2 bg-transparent focus:ring-0 focus:outline-none border-none appearance-none"} 
       >
         <option value="en">EN</option>
         <option value="de">DE</option>
-      </select>
+      </Select>
             </div>
         </div>
     </div>
